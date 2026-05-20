7c1252ca2d7a3b9d19d0815b3dfadc21c0b383a2
Hansil <chapadiyahansil@gmail.com>
2026-05-20 23:12:34 +0530

    feat: UserPage Css applied

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,14 +1,31 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../store";
+import "../styles/UserPage.css";
 
 const UserPage = () => {
 
     const users = useSelector((state: RootState) => state.users.users);
     return (
-        <div>
+        <div className="user-container">
             {users.map((u) => (
-                <div key={u.id}>
-                    <h3>{u.firstName}</h3 >
+                <div className="user-card" key={u.id}>
+                    <div className="user-card__header">
+                        <img
+                            className="user-card__avatar"
+                            src={u.image}
+                            alt={`${u.firstName} ${u.lastName}`}
+                        />
+                        <div>
+                            <h3 className="user-card__name">{u.firstName} {u.lastName}</h3>
+                            <p className="user-card__role">{u.role}</p>
+                        </div>
+                    </div>
+                    <div className="user-card__body">
+                        <p><span>Email:</span> {u.email}</p>
+                        <p><span>Phone:</span> {u.phone}</p>
+                        <p><span>Company:</span> {u.company?.name}</p>
+                        <p><span>City:</span> {u.address?.city}</p>
+                    </div>
                 </div>
             ))}
         </div>
