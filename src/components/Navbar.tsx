d1f4760d646c513fadcc6bb7289f1b8076b77a10
Hansil <chapadiyahansil@gmail.com>
2026-05-20 22:11:29 +0530

    fix: Navbar UI enhanced

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,24 @@
 import { NavLink, useNavigate } from "react-router";
-import "./Navbar.css";
+import "../styles/Navbar.css";
 import type { AppDispatch } from "../store";
 import { useDispatch } from "react-redux";
 import { logout } from "../features/authSlice";
 import { getUser, setUserActive } from "../features/userSlice";
 import { getPosts, setPostActive } from "../features/postSlice";
 import { getProduct, setProductActive } from "../features/productSlice";
+import { useState } from "react";
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch<AppDispatch>();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleUser = () => {
         dispatch(setUserActive(true));
         dispatch(setPostActive(false));
         dispatch(setProductActive(false));
         dispatch(getUser());
+        setIsMenuOpen(false);
     }
 
     const handlePost = () => {
@@ -23,6 +26,7 @@ const Navbar = () => {
         dispatch(setUserActive(false));
         dispatch(setProductActive(false));
         dispatch(getPosts());
+        setIsMenuOpen(false);
     }
 
     const handleProduct = () => {
@@ -30,16 +34,28 @@ const Navbar = () => {
         dispatch(setPostActive(false));
         dispatch(setUserActive(false));
         dispatch(getProduct());
+        setIsMenuOpen(false);
     }
 
     return (
         <div className="container">
-            <ul className="nav-container">
+            <button
+                className="hamburger"
+                aria-label="Toggle navigation"
+                aria-expanded={isMenuOpen}
+                onClick={() => setIsMenuOpen((prev) => !prev)}
+            >
+                <span />
+                <span />
+                <span />
+            </button>
+
+            <ul className={`nav-container ${isMenuOpen ? "open" : ""}`}>
                 <li><NavLink to={"/"} className={'link'} onClick={() => { handleUser() }}>USERS</NavLink></li>
                 <li><NavLink to={"/"} className={'link'} onClick={() => { handlePost() }}>POSTS</NavLink></li>
                 <li><NavLink to={"/"} className={'link'} onClick={() => { handleProduct() }}>PRODUCTS</NavLink></li>
             </ul>
-            <button onClick={() => { dispatch(logout()); navigate('/login') }}>Logout</button>
+            <button className="logout" onClick={() => { dispatch(logout()); navigate('/login') }}>Logout</button>
         </div>
     )
 }
