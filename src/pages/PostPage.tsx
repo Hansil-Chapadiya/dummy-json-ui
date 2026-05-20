32b21f8158831e4b85346a19e0f43dc6d6a42aa3
Hansil <chapadiyahansil@gmail.com>
2026-05-20 23:15:27 +0530

    feat: PostPage Css applied

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,15 +1,27 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../store";
+import "../styles/PostPage.css";
 
 const PostPage = () => {
 
     const posts = useSelector((state: RootState) => state.posts.posts);
 
     return (
-        <div>
+        <div className="post-container">
             {posts.map((p) => (
-                <div key={p.id}>
-                    {p.title}
+                <div className="post-card" key={p.id}>
+                    <h3 className="post-title">{p.title}</h3>
+                    <p className="post-body">{p.body}</p>
+                    <div className="post-tags">
+                        {p.tags?.map((t) => (
+                            <span className="post-tag" key={t}>{t}</span>
+                        ))}
+                    </div>
+                    <div className="post-meta">
+                        <span>👍 {p.reactions?.likes ?? 0}</span>
+                        <span>👎 {p.reactions?.dislikes ?? 0}</span>
+                        <span>👁️ {p.views ?? 0}</span>
+                    </div>
                 </div>
             ))}
         </div>
